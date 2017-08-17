ba192515424b87e167998b32ce8d14c099135287
Pieter Joost van de Sande <pj@born2code.net>
2017-08-17 19:36:26 +0100

    cluster: elect only once per elect request

--- cluster/etcd/etcd.go
+++ cluster/etcd/etcd.go
@@ -6,6 +6,7 @@ import (
 	"golang.org/x/net/context"
 	"github.com/coreos/etcd/clientv3/concurrency"
 	"github.com/pjvds/streamdb/cluster"
+	"github.com/pkg/errors"
 )
 
 type EtcdElector struct{
@@ -27,7 +28,7 @@ func NewMasterElection(log *zap.Logger, client *clientv3.Client, key, value stri
 
 type MasterLock struct{
 	session *concurrency.Session
-	election concurrency.Election
+	election *concurrency.Election
 }
 
 func (this *MasterLock) Lost() <-chan struct{} {
@@ -67,46 +68,33 @@ func (this *EtcdElector) follow(ctx context.Context, changes chan string) {
 }
 
 func (this *EtcdElector) Elect(ctx context.Context) (cluster.Election, error) {
+	session, err := concurrency.NewSession(this.client, concurrency.WithContext(ctx), concurrency.WithTTL(5))
+	if err != nil {
+		return nil, errors.WithMessage(err,"etcd concurrency session creation failed")
+	}
+
+	log := this.log.With(zap.Any("lease", session.Lease()))
+
 	won := make(chan cluster.MasterLock)
-	this.log.Debug("elector started")
 
 	go func() {
 		defer close(won)
 
-		for{
-			// TODO: rerun on error
-			session, err := concurrency.NewSession(this.client, concurrency.WithContext(ctx), concurrency.WithTTL(5))
-			if err != nil {
-				this.log.Error("failed to create concurrency session", zap.Error(err))
-				return
-			}
+		log.Debug("etcd election started", zap.String("key", this.key))
 
-			for {
-				election := concurrency.NewElection(session, this.key)
-				if err := election.Campaign(ctx, this.value); err != nil {
-					this.log.Warn("etcd election campaign failed", zap.Error(err))
-					return
-				}
-				this.log.Debug("etcd election campaign won", zap.String("key", this.key), zap.String("value", this.value))
-
-				select {
-				case <-ctx.Done():
-					session.Close();
-
-					this.log.Debug("master election run stopped because context closed")
-					return
-				case won <- &MasterLock{session: session}:
-				case <-session.Done():
-					break
-				}
-
-				// block till be lost our session
-				<-session.Done()
-				break
-			}
+		election := concurrency.NewElection(session, this.key)
+		if err := election.Campaign(ctx, this.value); err != nil {
+			log.Warn("etcd election campaign failed", zap.Error(err))
+			return
 		}
 
+		log.Debug("etcd election campaign won", zap.String("key", this.key), zap.String("value", this.value))
 
+		select {
+		case <-ctx.Done():
+			session.Close()
+		case won <- &MasterLock{session, election}:
+		}
 	}()
 
 	return won, nil
